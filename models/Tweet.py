eeae4ffca9a72e9459fdd85c3576db2bbc4ef873
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-08 18:43:41 -0300

    Adicionado model de Tweet

---
+++ models/Tweet.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Tweet(db.Model):
+    __tablename__ = "TB_TWEET"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id_user = db.Column(db.Integer, foreign_key = True)
+    tweet = db.Column(db.String(280))
+    tweeted_at = db.Column(db.DateTime)
+
+    def __init__(self, id_user, tweet, tweeted_at):
+        self.id_user = id_user
+        self.tweet = tweet
+        self.tweeted_at = tweeted_at 
