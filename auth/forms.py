733364d2fb7836e736a941ac361e6f20ba9ae9f2
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-08 17:27:28 -0300

    Adicionado métodos de validação de registro

--- auth/forms.py
+++ auth/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+from models.User import User
 
 class LoginForm(FlaskForm):
     email = StringField('', validators=[DataRequired()])
@@ -11,3 +13,15 @@ class RegisterForm(FlaskForm):
     username = StringField('', validators=[DataRequired()])
     email = StringField('', validators=[DataRequired()])
     password = PasswordField('', validators=[DataRequired()])
+
+    def validate_email(self, email):
+        if User.query.filter_by(email = email.data).first():
+            return False
+
+        return True
+    
+    def validate_username(self, username):
+        if User.query.filter_by(username = username.data).first():
+            return False
+            
+        return True
