771d59e720186db5170230a8c2c232897879c118
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-05-09 12:28:06 -0300

    Ajustes no layout na página de perfil

--- models/User.py
+++ models/User.py
@@ -8,18 +8,20 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(50))
     username = db.Column(db.String(50))
     email = db.Column(db.String(255))
+    bio = db.Column(db.String(150))
     password = db.Column(db.String(255))
     birth_date = db.Column(db.Date)
     created_at = db.Column(db.DateTime)
     verified = db.Column(db.Boolean)
 
-    def __init__(self, name, username, email, password, birth_date, created_at):
+    def __init__(self, name, username, email, password, birth_date, created_at, bio):
         self.name = name
         self.username = username
         self.email = email
         self.password = password
         self.birth_date = birth_date
         self.created_at = created_at
+        self.bio = bio
 
 
 @login_manager.user_loader
